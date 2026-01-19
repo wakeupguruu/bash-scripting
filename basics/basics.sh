fea99722489a8b16270c2a9e5965f5be338cc5a7
wakeupguruu <vyasguru19@gmail.com>
2026-01-19 08:44:09 +0000

    chore: add basic learning script structure

---
+++ basics/basics.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# The shebang tells the kernel which interpreter to use, regardless of the user’s current shell, unless an interpreter is explicitly specified
+
+echo "Hello World"
+echo "($ls)"
+echo ls
+echo "My current working directory is: $(pwd)"
